36e54a1d31061c60e003d76516d5cb6f6556128b
Bala Nagendra <68959202+ybnagendra@users.noreply.github.com>
2020-11-19 21:08:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-import time
-import dlmode, oledScreens, configure
+import dlmode, oledScreens
 
-oled =oledScreens.OLEDSCREENS()
+oled = oledScreens.OLEDSCREENS()
 
 if __name__ == "__main__":
     oled.WelcomeMessage()
     while True:
         oled.ModesOfDataLogger()
-        key='2'
+        key = '2'
         if (key == '1'):
             print("----------------------CONF MODE----------------------")
             configure.DL_SETTINGS()
             dlmode.RUN_MODE()
         elif (key == '2'):
             print("----------------------RUN MODE----------------------")
-            dlmode.RUN_MODE()
+            dlmode.RUN_MODE()        
