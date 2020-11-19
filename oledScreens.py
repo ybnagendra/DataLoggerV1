9cb56238fc075be9fd9f728212c7d0fb59abf42d
Bala Nagendra <68959202+ybnagendra@users.noreply.github.com>
2020-11-19 22:37:45 +0530

    Update oledScreens.py

--- oledScreens.py
+++ oledScreens.py
@@ -68,6 +68,7 @@ class OLEDSCREENS:
         realPower = ''
         reactivePower = ''
         apparentPower = ''
+        
         try:
             x1 = resp1.index('V')
             x2 = resp1.index('A')
@@ -78,18 +79,6 @@ class OLEDSCREENS:
             current = resp1[x2 + 2:x3 - 1]
             power = resp1[x3 + 3:x4 - 1]
             resistance = resp1[x4 + 2:]
-
-            y1 = resp2.index('KW')
-            y2 = resp2.index('VAR')
-            y3 = resp2.index('KVA')
-
-            realPower = resp2[y1 + 3:y2 - 1]
-            reactivePower = resp2[y2 + 4:y3 - 1]
-            apparentPower = resp2[y3 + 4:]
-        except ValueError:
-            pass
-
-        try:
             oledExp.setCursor(1, 0)
             oledExp.write("VOLTAGE = " + str(voltage) + " V")
             oledExp.setCursor(2, 0)
@@ -98,68 +87,28 @@ class OLEDSCREENS:
             oledExp.write("POWER = " + str(power) + " KW")
             oledExp.setCursor(4, 0)
             oledExp.write("RES = " + str(resistance) + " Ohm")
+        except ValueError:
+            pass
+        except:
+            pass
+
+        try:
+            y1 = resp2.index('KW')
+            y2 = resp2.index('VAR')
+            y3 = resp2.index('KVA')
+            realPower = resp2[y1 + 3:y2 - 1]
+            reactivePower = resp2[y2 + 4:y3 - 1]
+            apparentPower = resp2[y3 + 4:]
             oledExp.setCursor(5, 0)
             oledExp.write("P = " + str(realPower) + " KW")
             oledExp.setCursor(6, 0)
             oledExp.write("Q = " + str(reactivePower) + " KVAR")
             oledExp.setCursor(7, 0)
             oledExp.write("S = " + str(apparentPower) + " KVA")
+        except ValueError:
+            pass
         except:
             pass
-
-    def WrongSelectionOfKey(self):
-        oledExp.clear()
-        oledExp.setCursor(3, 0)
-        oledExp.write("Invalid Selection")
-        oledExp.setCursor(4, 0)
-        oledExp.write("Select 1 or 2 or 3")
-        time.sleep(2)
-        oledExp.clear()
-        self.ModesOfDataLogger()
-
-    def MenuOfConfigureMode(self):
-        oledExp.clear()
-        oledExp.setCursor(7, 0)
-        oledExp.write("CONFIGURATION MODE")
-        oledExp.setCursor(6, 0)
-        oledExp.write("*********************")
-        oledExp.setCursor(1, 0)
-        oledExp.write("1. SET DATE AND TIME ")
-        oledExp.setCursor(2, 0)
-        oledExp.write("2. To Edit Other Parameters")
-        oledExp.setCursor(3, 0)
-        oledExp.write("3. To Read Configuration")
-        oledExp.setCursor(4, 0)
-        oledExp.write("Your Selection: ")
-
-    def ConfigureMode_Page2(self):
-        oledExp.clear()
-        oledExp.setCursor(3, 0)
-        oledExp.write("Invalid Selection")
-        oledExp.setCursor(4, 0)
-        oledExp.write("Select 1 or 2 or 3")
-        time.sleep(2)
-        oledExp.clear()
-        self.MenuOfConfigureMode()
-
-    def ConnectToPC(self):
-        oledExp.setCursor(7, 0)
-        oledExp.write("CONFIGURATION MODE")
-        oledExp.setCursor(6, 0)
-        oledExp.write("*********************")
-        oledExp.setCursor(3, 0)
-        oledExp.write("Connect to PC/Laptop ")
-        oledExp.setCursor(4, 0)
-        oledExp.write("through USB Cable ")
-
-    def ConfDoneMsg(self):
-        oledExp.clear()
-        oledExp.setCursor(7, 0)
-        oledExp.write("CONFIGURATION MODE")
-        oledExp.setCursor(6, 0)
-        oledExp.write("*********************")
-        oledExp.setCursor(3, 0)
-        oledExp.write("Configuartion done")
 
     def clear(self):
         try:
