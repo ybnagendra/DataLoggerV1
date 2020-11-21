b25621bae5a2d19f7b2f198b4a6a715f3b4c8f24
Bala Nagendra <68959202+ybnagendra@users.noreply.github.com>
2020-11-21 10:07:01 +0530

    Update oledScreens.py

--- oledScreens.py
+++ oledScreens.py
@@ -5,6 +5,7 @@ import time
 class OLEDSCREENS:
     def WelcomeMessage(self):
         try:
+            oledExp.clear()
             oledExp.setVerbosity(0)
             oledExp.driverInit()
             oledExp.clear()
@@ -13,12 +14,12 @@ class OLEDSCREENS:
             oledExp.setCursor(4, 0)
             oledExp.write("Data Logger Project")
             time.sleep(3)
-            oledExp.clear()
         except:
             pass
 
     def ModesOfDataLogger(self):
         try:
+            oledExp.clear()
             oledExp.setCursor(2, 0)
             oledExp.write("Modes of DataLogger:")
             oledExp.setCursor(3, 0)
@@ -28,7 +29,6 @@ class OLEDSCREENS:
             oledExp.setCursor(5, 0)
             oledExp.write("3. BACKUP MODE")
             time.sleep(3)
-            oledExp.clear()
         except:
             pass
 
@@ -43,6 +43,7 @@ class OLEDSCREENS:
 
     def CopyDoneMsg(self):
         try:
+            #oledExp.clear()
             oledExp.setCursor(4, 0)
             oledExp.write("copy Done")
             time.sleep(3)
@@ -58,6 +59,7 @@ class OLEDSCREENS:
             pass
 
     def DisplayAnalyserData(self, resp1, resp2):
+        '''
         # 01 Request Response Parameters
         voltage = ''
         current = ''
@@ -68,17 +70,25 @@ class OLEDSCREENS:
         realPower = ''
         reactivePower = ''
         apparentPower = ''
-        try:
 
+        '''
+        try:
             x1 = resp1.index('V')
             x2 = resp1.index('A')
             x3 = resp1.index('KW')
             x4 = resp1.index('R')
+            y1 = resp2.index('KW')
+            y2 = resp2.index('VAR')
+            y3 = resp2.index('KVA')
 
             voltage = resp1[x1 + 2:x2 - 1]
             current = resp1[x2 + 2:x3 - 1]
             power = resp1[x3 + 3:x4 - 1]
             resistance = resp1[x4 + 2:]
+            realPower = resp2[y1 + 3:y2 - 1]
+            reactivePower = resp2[y2 + 4:y3 - 1]
+            apparentPower = resp2[y3 + 4:]
+
             oledExp.setCursor(1, 0)
             oledExp.write("VOLTAGE = " + str(voltage) + " V")
             oledExp.setCursor(2, 0)
@@ -87,18 +97,6 @@ class OLEDSCREENS:
             oledExp.write("POWER = " + str(power) + " KW")
             oledExp.setCursor(4, 0)
             oledExp.write("RES = " + str(resistance) + " Ohm")
-        except ValueError:
-            self.HashOneResponsefailMsg()
-        except:
-            pass
-
-        try:
-            y1 = resp2.index('KW')
-            y2 = resp2.index('VAR')
-            y3 = resp2.index('KVA')
-            realPower = resp2[y1 + 3:y2 - 1]
-            reactivePower = resp2[y2 + 4:y3 - 1]
-            apparentPower = resp2[y3 + 4:]
             oledExp.setCursor(5, 0)
             oledExp.write("P = " + str(realPower) + " KW")
             oledExp.setCursor(6, 0)
@@ -106,25 +104,48 @@ class OLEDSCREENS:
             oledExp.setCursor(7, 0)
             oledExp.write("S = " + str(apparentPower) + " KVA")
         except ValueError:
-            self.HashTwoResponsefailMsg()
+            self.AnalyserResponseFailMsg()
         except:
             pass
 
-    def HashOneResponsefailMsg(self):
+    def AnalyserResponseFailMsg(self):
         try:
             oledExp.clear()
             oledExp.setCursor(3,0)
-            oledExp.write("#01 Response not received from analyzer")
+            oledExp.write("#Analyser Response not")
+            oledExp.setCursor(4, 0)
+            oledExp.write("received")
         except:
             pass
 
-    def HashTwoResponsefailMsg(self):
+    def MenuofConfigureMode(self):
         try:
             oledExp.clear()
-            oledExp.setCursor(3,0)
-            oledExp.write("#02 Response not received from analyzer")
+            oledExp.setCursor(0, 0)
+            oledExp.write("CONFIGURATION MODE: ")
+            oledExp.setCursor(2, 0)
+            oledExp.write("1. SET DATE AND TIME")
+            oledExp.setCursor(3, 0)
+            oledExp.write("2. EDIT CONFIGURATION")
+            oledExp.setCursor(4, 0)
+            oledExp.write("3. READ CONFIGURATION")
         except:
             pass
+
+    def DisplayConfigData(self,x,y,z,w):
+        try:
+            oledExp.clear()
+            oledExp.setCursor(1,0)
+            oledExp.write(x)
+            oledExp.setCursor(2,0)
+            oledExp.write(y)
+            oledExp.setCursor(3, 0)
+            oledExp.write(z)
+            oledExp.setCursor(4, 0)
+            oledExp.write(w)
+        except:
+            pass
+
 
     def clear(self):
         try:
